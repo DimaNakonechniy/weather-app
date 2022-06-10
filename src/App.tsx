a507c84b9841f1f5b2b75121e4c24538c456a870
Dmytro Nakonechnyi <dmytro.nak@team.anyforsoft.com>
2022-06-10 15:45:48 +0300

    Added Material UI library, updated main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
-const App = () => <div className="App">React app content</div>;
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const App = () => (
+  <Container maxWidth="sm">
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="h1">
+          Weather App
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </Container>
+);
 
 export default App;
