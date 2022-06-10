24220b28210e209c748e1c90b993dfd7231e8f27
Dmytro Nakonechnyi <dmytro.nak@team.anyforsoft.com>
2022-06-10 14:27:55 +0300

    Changed main App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-   React app
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App">React app</div>;
+};
 
 export default App;
